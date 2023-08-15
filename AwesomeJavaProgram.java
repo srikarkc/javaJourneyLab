3e5e23e1aa524c611b41a1e08e4b8f1e68093559
Srikar KC <sclifeisgreat@gmail.com>
2023-08-14 21:41:46 -0700

    Added an AwesomeJavaProgram

---
+++ AwesomeJavaProgram.java
@@ -0,0 +1,10 @@
+public class AwesomeJavaProgram {
+    public static void main(String[] args) {
+        System.out.println(printName("Srikar"));
+    }
+
+    private static String printName(String name) {
+        String outputString = "My name is " + name;
+        return outputString;
+    }
+}
