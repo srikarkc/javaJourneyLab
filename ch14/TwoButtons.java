b828173b2dd546261eb055a6519a033e4e7b623f
Srikar KC <sclifeisgreat@gmail.com>
2023-11-30 10:53:00 -0800

    Added code from ch14 code kitchen

--- ch14/TwoButtons.java
+++ ch14/TwoButtons.java
@@ -50,23 +50,23 @@ public class TwoButtons {
 }
 class MyDrawPanel extends JPanel {
     public void paintCompenent(Graphics g) {
-        g.setColor(Color.blue);
-        g.fillRect(20, 50, 100, 100);
-        // Graphics2D g2d = (Graphics2D) g;
+        // g.setColor(Color.blue);
+        // g.fillRect(20, 50, 100, 100);
+        Graphics2D g2d = (Graphics2D) g;
 
-        // Random random = new Random();
-        // int red = random.nextInt(256);
-        // int green = random.nextInt(256);
-        // int blue = random.nextInt(256);
-        // Color startColor = new Color(red, green, blue);
+        Random random = new Random();
+        int red = random.nextInt(256);
+        int green = random.nextInt(256);
+        int blue = random.nextInt(256);
+        Color startColor = new Color(red, green, blue);
         
-        // red = random.nextInt(256);
-        // green = random.nextInt(256);
-        // blue = random.nextInt(256);
-        // Color endColor = new Color(red, green, blue);
+        red = random.nextInt(256);
+        green = random.nextInt(256);
+        blue = random.nextInt(256);
+        Color endColor = new Color(red, green, blue);
 
-        // GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
-        // g2d.setPaint(gradient);
-        // g2d.fillOval(70, 70, 100, 100);
+        GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70, 70, 100, 100)n
     }
 }
